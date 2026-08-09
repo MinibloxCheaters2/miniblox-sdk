fec49650e51d91318ff22fe897933bbf6ee8abe0
bab <bab@noreply.codeberg.org>
2026-08-09 10:50:20 -0500

    refactor: forgor to export

--- src/game/index.d.ts
+++ src/game/index.d.ts
@@ -2,6 +2,7 @@ export * from "./controls";
 export * from "./game";
 export * from "./gamemode";
 export * from "./gameScene";
+export * from "./skinManager";
 export * from "./hud";
 export * from "./options";
 export * from "./team";
